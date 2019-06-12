0a4c897e43b3f01821dac77a582109894b52b76e
guci <guci314@gmail.com>
2019-06-12 17:26:29 +0800

    ssss

--- solrApi.py
+++ solrApi.py
@@ -2,12 +2,16 @@ from SolrClient import SolrClient
 import requests
 import os
 class SolrApi(SolrClient):
+    def __init__(self,host):
+        #self.suffixs=["md","xml","txt","scala","java","cs","ts","js","ipynb","py","php","rtf","rdf","cpp","c","h","json","csv","ppt","pptx","ots","odt","odp","ods","ott","otp","pdf","html","sbt","docx","doc","xls","xlsx"]
+        SolrClient.__init__(self,host)
+        self.suffixs=["md","txt","rtf","ppt","pptx","ots","odt","odp","ods","ott","otp","pdf","html","docx","doc","xls","xlsx"]
     def func(self):
         print(self.host)
     def postFile(self,collection,file):
         posturl = self.host+"/"+collection+"/update/extract"
         p={}
-        p["commit"]="true"
+        p["commit"]="false"
         p["literal.id"]=file
         p["literal.attr_resourcename"]=file
         suffix=os.path.splitext(file)[1]
@@ -17,7 +21,7 @@ class SolrApi(SolrClient):
             payload = open(file, "rb").read()
             r = requests.post(posturl, data=payload, params=p)
             return r
-        except e:
+        except Exception as e:
             print(e)
     def getAllId(self,collection):
         ids=[]
@@ -25,3 +29,30 @@ class SolrApi(SolrClient):
             for doc in res.docs:
                 ids.append(doc['id'])
         return ids
+
+    def indexFolder(self,collection,folder):
+        '''
+        索引文件夹
+        '''
+        
+        #suffixs=[".java"]
+        counter=0
+        for dirpath, dirnames, filenames in os.walk(folder):
+            for filepath in filenames:
+                name=os.path.join(dirpath, filepath)
+                suffix=os.path.splitext(name)[1]
+                suffix=suffix.strip(".")
+                try:
+                    i=self.suffixs.index(suffix)
+                    self.postFile(collection,name)
+                    counter=counter+1
+                    if (counter % 500 == 0):
+                        self.commit(collection)
+                        print(counter)
+                except Exception as e:
+                    pass
+        self.commit(collection)
+
+solr=SolrApi('http://127.0.0.1:8983/solr')
+solr.indexFolder('files','/home/guci/share/baidunetdisk')
+print('finished')
--- test2.py
+++ test2.py
@@ -16,4 +16,39 @@ def f(x:int)->int:
 y=f(7)
 print(y)
 
+#%%
+s='ppppppppppp'
+print(s)
+#%%
+class X:
+    def f(self):
+        print('dfdf')
+x=X()
+x.f()
 
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+x_index = 2
+y_index = 1
+
+# this formatter will label the colorbar with the correct target names
+formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
+
+plt.scatter(iris.data[:, x_index], iris.data[:, y_index],
+            c=iris.target, cmap=plt.cm.get_cmap('RdYlBu', 3))
+plt.colorbar(ticks=[0, 1, 2], format=formatter)
+plt.clim(-0.5, 2.5)
+plt.xlabel(iris.feature_names[x_index])
+plt.ylabel(iris.feature_names[y_index]);
+
+#%%
+x=np.arange(10)
+x+=10
+y=np.sum(x)
+y=np.mean(x)
+print(y)
+#%%
